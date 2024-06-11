ce2b58d35a8e414505b1d581678de5fa43609de6
HMarchiori <hmarchiori2003@gmail.com>
2024-06-11 18:57:19 -0300

    omg

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,41 @@
 import java.rmi.registry.Registry;
 
 public class Cliente {
-    private InterfaceServidor
+    private InterfaceServidor servidor;
+    private StatusJogo statusJogo;
+    private String clientId;
+
+    private Cliente(String enderecoServidor) {
+        try {
+            Registry registry = java.rmi.registry.LocateRegistry.getRegistry(enderecoServidor);
+            servidor = (InterfaceServidor) registry.lookup("servidor");
+            this.clientId = java.util.UUID.randomUUID().toString();
+            servidor.registrarCliente(clientId);
+            new Thread(this::atualizarStatusJogo).start();
+        } catch (Exception e) {
+            System.err.println("Erro: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void atualizarStatusJogo() {
+        while (true) {
+            try {
+                statusJogo = servidor.getStatusJogo();
+                System.out.println("Status do jogo: " + statusJogo);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.err.println("Erro: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso: java Cliente <endereco_servidor>");
+            System.exit(1);
+        }
+        new Cliente(args[0]);
+    }
 }
